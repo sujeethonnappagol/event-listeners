605ffe3b26920f829f87102f309235d0c3b5d08f
sujeet <sujeethonnappagol3@gmail.com>
2025-09-27 23:38:45 +0530

    first file commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//creating listner
+//addEventListeners
+
+
+// let btn = document.getElementById("btn");
+// function test(){
+//     console.log("button clicked");
+// }
+// btn.addEventListener('click',test)
+
+// btn.addEventListener('click', (e)=>{
+//     console.log(e.target.innerText + ' button clicked');
+// })
